f6d1d40ed291ba3b4f2b325ebc2c41e552623540
koicode-official <koicode@koicode.co.kr>
2023-07-19 16:47:36 +0900

    logout update

--- src/component/mypage/MypageComponent.js
+++ src/component/mypage/MypageComponent.js
@@ -1,7 +1,11 @@
 "use client"
 import styled from "styled-components"
-import { CommonWrapper } from "../common/CommonComponent";
-import MypageMenuList from "./MypageMenuList";
+import { CommonButton, CommonWrapper } from "../common/CommonComponent";
+import { useQuery } from "react-query";
+import useCustomAxios from "@/utils/UseCustomAxios";
+import { useRouter } from "next/navigation";
+import { common } from "../../../public/js/common";
+// import MypageMenuList from "./MypageMenuList";
 const MypageWrapper = styled(CommonWrapper)`
 `
 const MypageContainer = styled.div`
@@ -12,13 +16,55 @@ const MypageContainer = styled.div`
   }
 `
 
+const LogoutButtonContainer = styled.div`
+  display: flex ;
+  justify-content: center ;
+  width: 100%;
+`
+
+const LogoutButton = styled(CommonButton)`
+  background-color: var(--color-set02);
+  color: var(--color-set07);
+`
 function MypageComponent() {
+  const axios = useCustomAxios();
+  const router = useRouter();
+
+  const logOut = async () => {
+    return await axios({
+      method: "GET",
+      withCredentials: true,
+      url: `${process.env.NEXT_PUBLIC_API_SERVER}/login/logout`,
+    })
+  }
+
+  const { refetch: logOutRefetch } = useQuery("logOut", logOut, {
+    enabled: false,
+    onSuccess: response => {
+      if (response.data.status === "success") {
+        common.setItemWithExpireTime("loggedIn", false, 0);
+        router.push("/");
+      }
+    },
+    onError: error => {
+      console.log("Error Occured : ", error);
+    }
+  });
+
+  const handleLogOut = () => {
+    logOutRefetch();
+  }
   return (
     <MypageWrapper>
       <MypageContainer>
         <h2>개발 예정</h2>
         {/* <MypageMenuList></MypageMenuList> */}
       </MypageContainer>
+      <LogoutButtonContainer>
+        <LogoutButton onClick={handleLogOut}>
+          로그아웃
+        </LogoutButton>
+      </LogoutButtonContainer>
     </MypageWrapper>
   );
 }
